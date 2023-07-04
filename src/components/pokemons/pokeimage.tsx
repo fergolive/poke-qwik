2f7b200a47c9fcbeb329e93609334989f6a93290
Fergolive <garciaolivo89@gmail.com>
2023-07-04 19:26:46 -0300

    first commit

---
+++ src/components/pokemons/pokeimage.tsx
@@ -0,0 +1,35 @@
+import { component$, useSignal, useTask$ } from "@builder.io/qwik"
+
+interface Props{
+    id:number
+    back:boolean
+}
+
+export const PokeImage = component$(({id,back=false}:Props) => {
+
+const imageLoaded=useSignal(false);
+useTask$(({track})=>{
+    track(()=>id)
+    imageLoaded.value=false
+})
+
+  return (
+    <>
+        {!imageLoaded.value && <span>loading...</span>}
+        <img 
+        width="130" 
+        height="130" 
+        src={
+             back ?
+            `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+            :
+            `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`
+            } 
+        alt=""
+        onLoad$={()=>imageLoaded.value=true} 
+        class={{'hidden':!imageLoaded.value}}
+        />
+    </>
+  )
+})
+
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,8 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
+
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "~/components/starter/header/header";
-import Footer from "~/components/starter/footer/footer";
+import Navbar from "~/components/shared/navbar/navbar";
 
 import styles from "./styles.css?inline";
 
@@ -18,21 +17,16 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
 
 export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
+      <Navbar />
       <main>
         <Slot />
       </main>
-      <Footer />
+     
     </>
   );
 });
